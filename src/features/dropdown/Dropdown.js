ed6c6697286f35f0399cf76a7bfee29d24573d70
charles jeremiah <charlesjeremiah89@yahoo.com>
2022-05-01 15:41:45 +0100

    .

--- src/features/dropdown/Dropdown.js
+++ src/features/dropdown/Dropdown.js
@@ -1,18 +1,34 @@
 import { Wrapper } from "./Dropdown.styles";
 import { FaTimes, FaCheck } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { filter } from "../login/loginSlice";
 
 const Dropdown = () => {
+  const dispatch = useDispatch();
+
   return (
     <Wrapper className="dropdown">
       <div className="dropdown_menu">
         <header className="dropdown_header">
           <span className="dropdown_title">Select order</span>
-          <button className="dropdown_close" type="button">
+          <button
+            className="dropdown_close"
+            type="button"
+            onClick={() => {
+              onShowMenu(false);
+            }}
+          >
             <FaTimes />
           </button>
         </header>
         <ul className="dropdown_list">
-          <li className="dropdown_item">
+          <li
+            className="dropdown_item"
+            onClick={() => {
+              dispatch(filter(true));
+              onShowMenu(false);
+            }}
+          >
             <FaCheck className="dropdown_icon" />
             <span>Name</span>
           </li>
