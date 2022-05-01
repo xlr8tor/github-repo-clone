beba56d44383d796084e7722b971eae380ebf597
charles jeremiah <charlesjeremiah89@yahoo.com>
2022-05-01 01:15:05 +0100

    route change

--- src/oauthConfig.js
+++ src/oauthConfig.js
@@ -2,6 +2,6 @@ const authEndpoint = "https://github.com/login/oauth/authorize";
 
 export const getAuthorizeHref = () => {
   const clientId = "6c1f90610f8c333f562f";
-  const redirectUri = "https://github-repo-task/login";
+  const redirectUri = "https://github-repo-clone-task.vercel.app/login";
   return `${authEndpoint}?scope=user&client_id=${clientId}&redirect_uri=${redirectUri}`;
 };
